bc1e996eb54b5ebfbd2def3360df66ab6dc4c246
Lucas Epps <lucasbepps@gmail.com>
2018-05-08 20:07:35 -0600

    Added a Procfile

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Cheese Chili Burger');
